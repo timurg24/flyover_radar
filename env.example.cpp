144d7606700ab61524c6f2b56249e8995785c00b
Timur Gabdrakhmanov <90166542+timurg24@users.noreply.github.com>
2026-06-14 13:02:12 -0500

    Add configuration file for environment settings

---
+++ env.example.cpp
@@ -0,0 +1,49 @@
+#include "env.h"
+#include <math.h>
+
+// =====================
+//   Config and Values
+// =====================
+
+// WiFi
+const char* wifiName = "WIFI NAME HERE!!";
+const char* wifiPassword = "super cool password";
+
+// OpenSky
+const char* clientID = "OpenSky Client ID goes here";
+const char* clientSecret = "is the secret in the room with us";
+int openSkyInterval = 5000;
+
+// Location
+// Change only this VVVV
+#define HOME_LAT 46.97313
+#define HOME_LON 7.94648
+#define BOX_RADIUS_MILES 3.0
+
+// Do not change VV
+
+#define MILES_PER_LAT_DEGREE 69.0
+#define DEG_TO_RAD 0.017453292519943295
+
+#define LAT_DELTA (BOX_RADIUS_MILES / MILES_PER_LAT_DEGREE)
+#define LON_DELTA (BOX_RADIUS_MILES / (MILES_PER_LAT_DEGREE * cos(HOME_LAT * DEG_TO_RAD)))
+
+LocationBounds bounds = {
+  HOME_LAT - LAT_DELTA,  // lamin - minimum latitude, bottom
+  HOME_LAT + LAT_DELTA,  // lamax - maximum latitude, top
+  HOME_LON - LON_DELTA,  // lomin - minimum longitude, left
+  HOME_LON + LON_DELTA   // lomax - maximum longitude, right
+};
+
+// Do not change ^^
+
+// Web interface
+int interfacePort = 24001;
+
+// Alerts
+int alertsCount = 51;
+AircraftData alerts[] = {
+  {"a310"}, {"an12"}, {"a124"}, {"a148"}, {"an26"}, {"an32"}, {"v22"}, {"b742"}, {"b743"}, {"b74s"}, {"b52"}, {"c135"}, {"c17"}, {"blcf"}, {"e3tf"}, {"e6"}, {"k35r"}, {"p8"}, {"r135"}, {"dc3"},
+  {"eufi"}, {"f16"}, {"il62"}, {"il76"}, {"il96"}, {"c5m"}, {"c130"}, {"c130j"}, {"f22"}, {"f35"}, {"p3"}, {"u2"}, {"dc10"}, {"f15"}, {"f18"}, {"md11"}, {"md81"},{"md82"},{"md83"},{"md87"},{"md88"},
+  {"md90"},{"hawk"},{"t38"},{"p51"}, {"su95"}, {"t154"}, {"b1"}, {"b2"}
+};
